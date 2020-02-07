43210ed7f0b55f7ff3fb4e6ad8d10509fd9c030d
Roberto Arruda <roberto0arruda@hotmail.com>
2020-02-07 16:56:01 -0400

    Trabalhando com Eventos class EventEmitter

---
+++ 03-events/index.js
@@ -0,0 +1,18 @@
+const EventEmitter = require('events')
+
+class MeuEmissor extends EventEmitter { }
+
+const meuEmissor = new MeuEmissor()
+const nomeEvento = 'usuario:click'
+
+meuEmissor.on(nomeEvento, function (click) {
+    console.log('um usuario clicou', click)
+})
+
+meuEmissor.emit(nomeEvento, 'na barra de rolagem')
+meuEmissor.emit(nomeEvento, 'no botão ok ')
+
+let count = 0
+setInterval(() => {
+    meuEmissor.emit(nomeEvento, 'no botão ok ' + (count++))
+}, 1000);
