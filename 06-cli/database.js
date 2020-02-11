1ab5c91704fa4cd076ba4b089fe3d9b483e03d65
Roberto Arruda <roberto0arruda@hotmail.com>
2020-02-11 16:04:52 -0400

    Criando CLI test cadastrar

--- 06-cli/database.js
+++ 06-cli/database.js
@@ -1,7 +1,8 @@
-const { readFile } = require("fs")
+const { readFile, writeFile } = require("fs")
 const { promisify } = require("util")
 
 const readFileAsync = promisify(readFile)
+const writeFileAsync = promisify(writeFile)
 
 // outra forma de obter dados do Json
 // const dadosJson = require('./herois.json')
@@ -16,10 +17,26 @@ class Database {
         return JSON.parse(arquivo.toString())
     }
 
+    async escreverArquivo(dados) {
+        await writeFileAsync(this.NOME_ARQUIVO, JSON.stringify(dados, null))
+        return true;
+    }
+
     async listar(id) {
         const dados = await this.obterDadosArquivo()
         const dadosFiltrado = dados.filter(item => (id ? item.id === id : true))
         return dadosFiltrado
+    }
+
+    async cadastrar(heroi) {
+        const dados = await this.obterDadosArquivo()
+        const id = heroi.id <= 2 ? heroi.id : Date.now()
+
+        const heroiComId = { id, ...heroi }
+        const dadosFinal = [...dados, heroiComId]
+
+        const cadastrou = await this.escreverArquivo(dadosFinal)
+        return cadastrou;
     }
 }
 
--- 06-cli/test.js
+++ 06-cli/test.js
@@ -8,12 +8,23 @@ const DEFAULT_ITEM = {
 }
 
 describe('Suite de teste, manipulação de heroi', () => {
+    before(async () => {
+        await database.cadastrar(DEFAULT_ITEM)
+    })
 
     it('deve pesquisar um heroi pelo id usando arquivos', async () => {
         const expected = DEFAULT_ITEM
         const [resultado] = await database.listar(expected.id)
 
         deepEqual(resultado, expected)
+    })
+
+    it('deve cadastrar um novo heroi usando arquivos', async () => {
+        const expected = { id: 2, nome: 'Batman', poder: 'Cash' }
+        const cadastrou = await database.cadastrar(expected)
+        const [atual] = await database.listar(expected.id)
+
+        deepEqual(atual, expected)
     })
 
 })
